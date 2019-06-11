2d56d591972c66ce82b0ed97e15bf7dfb93ae8d6
AlbenLiu <51562825+AlbenLiu@users.noreply.github.com>
2019-06-11 09:16:06 +0800

    Add files via upload

---
+++ newegg/src/api/data.php
@@ -0,0 +1,137 @@
+<?php
+    $guide1Navdata = [
+        [   
+            "index"=>1,
+            "brand"=>"Bose",
+            "content"=>"Bose SoundSport Free真无线耳机限量版-绚蓝紫",
+            "image"=>"guide1nav01",
+            "price"=>"1199.00",
+            "type"=>"SoundSport Free 绚蓝紫"
+        ],
+        [
+            "index"=>2,
+            "brand"=>"FUJIFILM 富士",
+            "content"=>"富士 FUJIFILM X-T20 XC 15-45 微单电套机 银色",
+            "image"=>"guide1nav02",
+            "price"=>"4999.00",
+            "type"=>"X-T20 15-45 银"
+        ],
+        [
+            "index"=>3,
+            "brand"=>"HUAWEI 华为",
+            "content"=>"华为 HUAWEI Mate20 Pro (UD) 8GB+128GB宝石蓝全网通双4G手机",
+            "image"=>"guide1nav03",
+            "price"=>"5499.00",
+            "type"=>"Mate 20 Pro (UD) 8GB+128GB宝石蓝"
+        ],
+        [
+            "index"=>4,
+            "brand"=>"HUAWEI 华为",
+            "content"=>"华为 HUAWEI Mate20 Pro (UD) 8GB+128GB宝石蓝全网通双4G手机",
+            "image"=>"guide1nav04",
+            "price"=>"4499.00",
+            "type"=>"Mate 20 X 6GB+128GB宝石蓝"
+        ],
+        [
+            "index"=>5,
+            "brand"=>"Apple 苹果",
+            "content"=>"Apple MacBook Pro 13.3英寸笔记本电脑 银色 Core i5/8GB内存/128GB硬盘 MPXR2CH/A 2017款",
+            "image"=>"guide1nav05",
+            "price"=>"8988.00",
+            "type"=>"MPXR2CH/A"
+        ],
+        [
+            "index"=>6,
+            "brand"=>"iRobot",
+            "content"=>"iRobot 艾罗伯特 iRobot 529 扫地机器人智能家用全自动清洁吸尘器扫地机",
+            "image"=>"guide1nav06",
+            "price"=>"999.00",
+            "type"=>"iRobot 529"
+        ],
+        [
+            "index"=>7,
+            "brand"=>"Bobot",
+            "content"=>"Bobot 智能电动拖地机 拖把 电拖把 家用 无线 手持 旋转 打蜡 擦地 Mop8600",
+            "image"=>"guide1nav07",
+            "price"=>"1099.00",
+            "type"=>"Mop8600"
+        ],
+        [
+            "index"=>8,
+            "brand"=>"SONY 索尼",
+            "content"=>"索尼WI-1000X 颈挂式降噪蓝牙耳机 WI-1000X黑色",
+            "image"=>"guide1nav08",
+            "price"=>"1399.00",
+            "type"=>"WI-1000X 颈挂式降噪蓝牙耳机 黑色"
+        ],
+        [
+            "index"=>9,
+            "brand"=>"Logitech 罗技",
+            "content"=>"Logitech 罗技 G900 有线/无线 双模式 游戏鼠标 RGB 鼠标",
+            "image"=>"guide1nav09",
+            "price"=>"795.00",
+            "type"=>"G900"
+        ],
+        [
+            "index"=>10,
+            "brand"=>"OLYMPUS 奥林巴斯",
+            "content"=>"Olympus 奥林巴斯 E-M1 Mark II(12-40mmPRO)微型单电套机",
+            "image"=>"guide1nav10",
+            "price"=>"13299.00",
+            "type"=>"E-M1 Mark II 12-40"
+        ],
+        [
+            "index"=>11,
+            "brand"=>"HUAWEI 华为",
+            "content"=>"HUAWEI 华为 MateBook X 13英寸 轻薄笔记本电脑(i5-7200U 8G 256G Win10 内含拓展坞)金",
+            "image"=>"guide1nav11",
+            "price"=>"6999.00",
+            "type"=>"MateBook X 13英寸 WT-W09B i5 8+256 金"
+        ],
+        [
+            "index"=>12,
+            "brand"=>"YAMAN 雅萌",
+            "content"=>"YAMAN 雅萌 射频仪脸部提拉导入导出洁面电子美容仪器 HRF-10T 国行",
+            "image"=>"guide1nav12",
+            "price"=>"2299.00",
+            "type"=>"HRF-10T"
+        ],
+        [
+            "index"=>13,
+            "brand"=>"YAMAN 雅萌",
+            "content"=>"YAMAN 雅萌 射频仪脸部提拉导入导出洁面电子美容仪器 HRF-10T 国行",
+            "image"=>"guide1nav13",
+            "price"=>"2299.00",
+            "type"=>"HRF-10T"
+        ],
+        [
+            "index"=>14,
+            "brand"=>"SONY 索尼",
+            "content"=>"索尼(SONY) WH-1000XM2 Hi-Res无线智能降噪耳机 香槟金",
+            "image"=>"guide1nav14",
+            "price"=>"1499.00",
+            "type"=>"WH-1000XM2 香槟金"
+        ],
+        [
+            "index"=>15,
+            "brand"=>"HP 惠普",
+            "content"=>"惠普HP M477/377dw打印机 A4彩色激光打印机一体机 多功能复印扫描传真一体机 477fdw标配",
+            "image"=>"guide1nav15",
+            "price"=>"1499.00",
+            "type"=>"M477/377dw 477fdw标配"
+        ],
+        [
+            "index"=>16,
+            "brand"=>"HP 惠普",
+            "content"=>"惠普HP 7500扫描仪 a4 平板扫描仪 馈纸式扫描",
+            "image"=>"guide1nav16",
+            "price"=>"9899.00",
+            "type"=>"7500扫描仪 a4 平板扫描仪 馈纸式扫描"
+        ]
+
+
+    ]
+        
+            
+        
+?>
